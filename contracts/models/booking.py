8ada6b4592c3ff8c3ca396b8e6fe9b122e3117c1
Slawomir-Kwiatkowski <Slawomir.Kwiatkowski.eu@gmail.com>
2024-03-11 21:56:46 +0100

    Booking model - validators added

--- contracts/models/booking.py
+++ contracts/models/booking.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.core import validators
 from .contract import Contract
 
 
 class Booking(models.Model):
     contract = models.OneToOneField(Contract, on_delete=models.CASCADE)
-    pallets_actual = models.IntegerField()
+    pallets_actual = models.IntegerField(
+        validators=(
+            validators.MinValueValidator(1),
+            validators.MaxValueValidator(30, "Max number of pallets is 30"),
+        )
+    )
     driver_full_name = models.CharField(max_length=20)
     driver_phone_number = models.CharField(max_length=10)
     truck_reg_number = models.CharField(max_length=10)
