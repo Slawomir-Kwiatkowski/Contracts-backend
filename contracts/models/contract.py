2c07b7c14a0e4c2a7b74287f613e556b430db2a1
Slawomir-Kwiatkowski <Slawomir.Kwiatkowski.eu@gmail.com>
2024-03-11 21:49:45 +0100

    Contract model - validators added

--- contracts/models/contract.py
+++ contracts/models/contract.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core import validators
 from django.utils import timezone
 from users.models import ContractUser
 from .warehouse import Warehouse
@@ -13,6 +14,15 @@ class Contract(models.Model):
         OPEN = "open"
         ACCEPTED = "accepted"
         CANCELLED = "cancelled"
+
+    class DeliveryDateValidator:
+        def validate(value):
+            if value < timezone.now().date():
+                raise validators.ValidationError(
+                    "Date of delivery can't be earlier than date of order"
+                )
+            else:
+                return value
 
     def contractors_choices():
         contractors = ContractUser.objects.filter(profile="contractor").all()
@@ -32,9 +42,14 @@ class Contract(models.Model):
     # ContractUser, on_delete=models.PROTECT, related_name="+"
     # )
     date_of_order = models.DateTimeField(default=timezone.now)
-    date_of_delivery = models.DateField()
+    date_of_delivery = models.DateField(validators=(DeliveryDateValidator.validate,))
     time_of_delivery = models.TimeField()
-    pallets_planned = models.IntegerField()
+    pallets_planned = models.IntegerField(
+        validators=(
+            validators.MinValueValidator(1),
+            validators.MaxValueValidator(30, "Max number of pallets is 30"),
+        )
+    )
     # warehouse = models.ForeignKey(Warehouse, on_delete=models.PROTECT)
     warehouse = models.CharField(choices=warehouses_choices, max_length=15)
     #
